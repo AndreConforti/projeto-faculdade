5cfb325247e5b5cc79b2aa179d93bc491c04f717
AndreConforti <andre.r.m.conforti@gmail.com>
2022-07-09 17:39:42 -0300

    Modelo logico criado
    Faltam implementar algumas tabelas.

---
+++ database/criacao_do_banco_e_tabelas.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------------------
+-- Schema bd_faculdade
+--
+-- Banco de dados criado para o gerenciamento de uma Faculdade, sendo utilizado para o aprendizado em SQL, Python, QT Designer, MySQL.
+-- -----------------------------------------------------
+CREATE DATABASE IF NOT EXISTS db_faculdade
+	DEFAULT CHARSET = utf8
+    DEFAULT COLLATE = utf8_general_ci;
+    
+USE db_faculdade;
+
