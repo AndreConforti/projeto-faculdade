35e31c801155c276c7d41868ee736768ddffe006
Andre R M Conforti <andre.r.m.conforti@gmail.com>
2022-07-11 16:51:08 -0300

    Iniciando os primeiros códigos

---
+++ codigos/principal.py
@@ -0,0 +1,7 @@
+from PyQt5 import QtWidgets, uic
+
+app = QtWidgets.QApplication([])
+
+# É NECESSÁRIO ASSOCIAR OS FORMULÁRIOS A UMA VERIÁVEL PARA PODER CHAMÁ-LOS NO SISTEMA
+
+telaLogin = uic.loadUi()
