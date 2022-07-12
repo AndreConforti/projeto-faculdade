89425c94d4d4d7db584b8dac11639655ea187896
Andre R M Conforti <andre.r.m.conforti@gmail.com>
2022-07-12 08:40:16 -0300

    problema solucionado
    Preciso executar pelo cmd, pois pelo terminal do VS Code dá erro.

--- codigos/principal.py
+++ codigos/principal.py
@@ -1,7 +1,14 @@
 from PyQt5 import QtWidgets, uic
+import os
 
 app = QtWidgets.QApplication([])
 
-# É NECESSÁRIO ASSOCIAR OS FORMULÁRIOS A UMA VERIÁVEL PARA PODER CHAMÁ-LOS NO SISTEMA
+# É NECESSÁRIO ASSOCIAR OS FORMULÁRIOS A UMA VARIÁVEL PARA PODER CHAMÁ-LOS NO SISTEMA
 
-telaLogin = uic.loadUi()
+telaLogin = uic.loadUi('../formularios/frmLoginUsuario.ui')
+
+
+
+
+telaLogin.show()
+app.exec()
